25ad2764c6bfbc02d99ff205ee91b0ea96d5219b
Timo Wesselmann <timo.wesselmann@gmail.com>
2023-02-02 20:13:14 +0100

    Initial Commit

---
+++ Meta Search/Meta_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meta_SearchApp.swift
+//  Meta Search
+//
+//  Created by Timo Wesselmann on 02.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Meta_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
